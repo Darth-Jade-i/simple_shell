c724f49fcffbcd9118bb70984d5a962e4b62e965
root <Solomon -Darth Jade-i- Okomowho>
2023-12-05 08:33:51 -0800

    edited betty

---
+++ Ms-betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a simple message to the console.
+ *
+  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Hello, Ms. Betty!\n");
+	return (0);
+}
+
